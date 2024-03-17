4463c4d1a109402efd582416038ae1c17322e39f
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2024-03-17 17:17:52 +0100

    rewrote the autocomplete functionality

--- lib/screens/views/pickup_location_destination/autocomplete_widget.dart
+++ lib/screens/views/pickup_location_destination/autocomplete_widget.dart
@@ -1,97 +1,20 @@
-import 'package:autocomplete_textfield/autocomplete_textfield.dart';
+//import 'package:autocomplete_textfield/autocomplete_textfield.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:truckhub/screens/constants/colors.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:truckhub/screens/constants/fontsizes.dart';
 import 'package:truckhub/screens/constants/fontweights.dart';
+import 'package:truckhub/screens/custom_widgets/text_widget.dart';
 import 'package:truckhub/screens/custom_widgets/textfield_widget.dart';
-import 'package:truckhub/screens/utils/extensions.dart';
 import 'dart:developer' as marach show log;
 
+
 class GenericAutoCompleteWidget extends StatefulWidget {
-  final List<String> options;
-  final String hintText;
-  final Widget? leadingWidget, 
-  suffixIcon;
-
-  const GenericAutoCompleteWidget({
-    super.key,
-    required this.options,
-    required this.hintText,
-    this.leadingWidget,
-    this.suffixIcon
-  });
-
-  @override
-  State<GenericAutoCompleteWidget> createState() => _GenericAutoCompleteWidgetState();
-}
-
-class _GenericAutoCompleteWidgetState extends State<GenericAutoCompleteWidget> {
-  bool showSuffixIcon = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Autocomplete<String>(
-      optionsBuilder: (TextEditingValue textEditingValue) {
-        if(textEditingValue.text.isEmpty){
-          setState(() => showSuffixIcon = false);
-          return const Iterable.empty();
-        }
-        else{
-          setState(() => showSuffixIcon = true);
-          final displayedOptions = widget.options.where(
-            (option) => option.toLowerCase().contains(
-              textEditingValue.text.toLowerCase()
-            )
-          );
-          return displayedOptions;
-        }
-      },
-      displayStringForOption: (selectedOption) => selectedOption,
-      onSelected: (selectedOption){},
-      initialValue: null,
-      optionsViewBuilder: (context, onSelected, options){
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: options.map(
-            (option) => GestureDetector(
-              onTap: () => onSelected(option),
-              child: Text(option).decorateText(
-                color: blackColor,
-                fontWeight: fontWeight3,
-                fontSize: fontSize3
-              ),
-            )
-          ).toList(),
-        );
-      },
-      fieldViewBuilder:(context, textEditingController, focusNode, onFieldSubmitted) {
-        return GenericTextField(
-          suffixIcon: showSuffixIcon ? IconButton(
-            onPressed: (){/*implement the functionality of this iconButton here*/},
-            icon: const FaIcon(FontAwesomeIcons.circleXmark)
-          ) : null,
-          leadingWidget: widget.leadingWidget,
-          hintText: widget.hintText,
-          controller: textEditingController,
-          focusNode: focusNode,
-        );
-      },
-    );
-  }
-}
-
-
-
-
-class GenericAutoCompleteWidget2 extends StatefulWidget {
   final List<String> options;
   final String hintText;
   final TextEditingController controller;
   final Widget? leadingWidget, suffixIcon;
 
-  const GenericAutoCompleteWidget2({
+  const GenericAutoCompleteWidget({
     super.key,
     required this.options,
     required this.hintText,
@@ -101,68 +24,53 @@ class GenericAutoCompleteWidget2 extends StatefulWidget {
   });
 
   @override
-  State<GenericAutoCompleteWidget2> createState() => _GenericAutoCompleteWidget2State();
+  State<GenericAutoCompleteWidget> createState() => _GenericAutoCompleteWidget3State();
 }
 
-class _GenericAutoCompleteWidget2State extends State<GenericAutoCompleteWidget2> {
-  GlobalKey<AutoCompleteTextFieldState<String>> globalKey = GlobalKey();
+class _GenericAutoCompleteWidget3State extends State<GenericAutoCompleteWidget> {
   bool showSuffixIcon = false;
 
   @override
   Widget build(BuildContext context) {
-
-    return AutoCompleteTextField(
-      itemSubmitted: (suggestion){},
-      key: globalKey,
-      suggestions: widget.options,
-      itemBuilder:(context, suggestion) {
-        return Padding(
-          padding: const EdgeInsets.all(10),
-          child: Text(suggestion.toString()),
+    return TypeAheadField<String>(
+      itemBuilder:(context, String suggestion) {
+        return Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 5, 0, 5),
+            child: GenericText(
+              fontSize: fontSize3half,
+              fontWeight: fontWeight5,
+              text: suggestion
+            )
+          ),
         );
       },
-      itemSorter:(a, b) => 0,
-      itemFilter:(suggestion, query) {
-        return suggestion.toString().toLowerCase().contains(query.toLowerCase());
+      onSelected:(value) {},
+      suggestionsCallback:(search) {
+        if(search.isEmpty){
+          setState(() => showSuffixIcon = false);
+          return List.empty();
+        }
+        else{
+         setState(() => showSuffixIcon = true);
+          final displayedOptions = widget.options.where(
+            (option) => option.toLowerCase().contains(
+              search.toLowerCase()
+            )
+          );
+          return displayedOptions.toList();
+        }
       },
-      //textChanged: (data){},
-      submitOnSuggestionTap: false,
-      controller: widget.controller,
-      autocorrect: true,
-      decoration: InputDecoration(
-        contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-        isDense: true,
-        labelStyle: const TextStyle().decorateTextStyle(
-          color: blackColor.withOpacity(0.5),
-          fontWeight: fontWeight4,
-          fontSize: fontSize3half
-        ),
-        floatingLabelStyle: const TextStyle().decorateTextStyle(
-          color: blackColor.withOpacity(0.6),
-          fontWeight: fontWeight4,
-          fontSize: fontSize4,
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: const BorderSide(
-            color: greenColor
-          ),
-          borderRadius: BorderRadius.circular(50),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: const BorderSide(
-            color: blackColor
-          ),
-          borderRadius: BorderRadius.circular(50),
-        ),
-        label: Text(widget.hintText),
-        suffixIcon: widget.suffixIcon,
-        prefixIcon: widget.leadingWidget
-      ),
-      style: const TextStyle().decorateTextStyle(
-        color: blackColor.withOpacity(0.7),
-        fontWeight: fontWeight5,
-        fontSize: fontSize3half,
-      ),
+      builder: (context, controller, focusNode) {
+        return GenericTextField(
+          hintText: widget.hintText, 
+          controller: controller,
+          focusNode: focusNode,
+          leadingWidget: widget.leadingWidget,
+          suffixIcon: showSuffixIcon ? widget.suffixIcon : null,
+        );
+      },
     );
   }
 }
--- lib/screens/views/pickup_location_destination/pickup_locaton_and_destination_search_view.dart
+++ lib/screens/views/pickup_location_destination/pickup_locaton_and_destination_search_view.dart
@@ -1,94 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gap/gap.dart';
 import 'package:truckhub/screens/constants/strings.dart';
 import 'package:truckhub/screens/custom_widgets/annotated_region_widget.dart';
 import 'package:truckhub/screens/views/pickup_location_destination/autocomplete_widget.dart';
-import 'dart:developer' as marach show log;
 
-class PickupLocationAndDestinationSearchScreen extends StatefulWidget {
+
+class PickupLocationAndDestinationSearchScreen extends HookWidget {
   const PickupLocationAndDestinationSearchScreen({super.key});
 
   @override
-  State<PickupLocationAndDestinationSearchScreen> createState() => _PickupLocationAndDestinationSearchScreenState();
-}
-
-class _PickupLocationAndDestinationSearchScreenState extends State<PickupLocationAndDestinationSearchScreen> {
-  late ValueNotifier<bool> valueNotifier;
-  late TextEditingController controller1, controller2;
-  List<String> options = [
-    'Daniel',
-    'Emmanuel',
-    'James',
-    'Ginika',
-    'Tesimi',
-    'David',
-    'Jerome',
-    'Damilola',
-    'Badamosi'
-  ];
-
-  @override 
-  void initState(){
-    super.initState();
-    valueNotifier = ValueNotifier<bool>(false);
-    controller1 = TextEditingController();
-    controller2 = TextEditingController();
-
-    controller1.addListener(() { 
-      if(controller1.text.isEmpty){
-        valueNotifier.value = false;
-      }
-      else{
-        valueNotifier.value = true;
-      }
-    });
-  }
-
-  @override 
-  void dispose(){
-    valueNotifier.dispose();
-    controller1.dispose();
-    controller2.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    
+    List<String> options = [
+      'Daniel',
+      'Emmanuel',
+      'James',
+      'Ginika',
+      'Tesimi',
+      'David',
+      'Jerome',
+      'Damilola',
+      'Badamosi'
+    ];
+    final controller1 = useTextEditingController();
+    final controller2 = useTextEditingController();
     return GenericAnnotatedRegion(
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
+          child: ListView(
+            padding: EdgeInsets.zero,
             children: [
               const Gap(60),
-              ValueListenableBuilder(
-                valueListenable: valueNotifier,
-                builder: (_, value, __) {
-                  marach.log(value.toString());
-                  return GenericAutoCompleteWidget2(
-                    controller: controller1,
-                    leadingWidget: IconButton(
-                      onPressed: (){/*Implement the functionalityy of the leadingIcon here*/},
-                      icon: const FaIcon(FontAwesomeIcons.arrowLeftLong,)
-                    ),
-                    suffixIcon: IconButton(
-                      onPressed: (){/*implement the functionality of the trailingIcon here*/},
-                      icon: const FaIcon(FontAwesomeIcons.circleXmark)
-                    ),
-                    options: options, //Provide the options for the autocomplete
-                    hintText:pickupLocationString
-                  );
-                },
+              GenericAutoCompleteWidget(
+                controller: controller1,
+                leadingWidget: IconButton(
+                  onPressed: (){/*Implement the functionalityy of the leadingIcon here*/},
+                  icon: const FaIcon(FontAwesomeIcons.arrowLeftLong,)
+                ),
+                suffixIcon: IconButton(
+                  onPressed: (){/*implement the functionality of the trailingIcon here*/},
+                  icon: const FaIcon(FontAwesomeIcons.circleXmark)
+                ),
+                options: options, //Provide the options for the autocomplete
+                hintText:pickupLocationString,
               ),
 
               const Gap(10),
 
-              GenericAutoCompleteWidget2(
+              GenericAutoCompleteWidget(
                 controller: controller2,
+                suffixIcon: IconButton(
+                  onPressed: (){/*implement the functionality of the trailingIcon here*/},
+                  icon: const FaIcon(FontAwesomeIcons.circleXmark)
+                ),
                 options: options, //Provide the options for the autocomplete
                 hintText: destinationString
               )
