b6a920234dd8c286fa96ebdc7393154f379b2f9b
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2024-03-18 19:52:53 +0100

    Made some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-
-// firebase
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
 import 'package:truckhub/screens/constants/colors.dart';
 import 'package:truckhub/screens/constants/fontsizes.dart';
 import 'package:truckhub/screens/constants/fontweights.dart';
 import 'package:truckhub/screens/utils/extensions.dart';
-import 'package:truckhub/screens/views/check_svg.dart';
 import 'package:truckhub/screens/views/confirm_phone_number/pincode_view.dart';
 import 'package:truckhub/screens/views/home_screen/main_home_screen.dart';
 import 'package:truckhub/screens/views/home_screen/sub_view/account_view/booked_drivers.dart';
@@ -20,14 +14,11 @@ import 'package:truckhub/screens/views/home_screen/sub_view/google_map_view.dart
 import 'package:truckhub/screens/views/home_screen/sub_view/notifications/notification_view.dart';
 import 'package:truckhub/screens/views/landing_page_screen.dart';
 import 'package:truckhub/screens/views/pickup_location_destination/pickup_locaton_and_destination_search_view.dart';
-// import 'package:truckhub/screens/views/welcome_back_screen.dart';
-// import 'package:truckhub/screens/views/welcome_to_truckhub_screen.dart';
-// import 'features/splash_screen/splash_screen.dart';
+import 'package:truckhub/screens/views/welcome_back_screen.dart';
+import 'package:truckhub/screens/views/welcome_to_truckhub_screen.dart';
+import 'features/splash_screen/splash_screen.dart';
 
-Future<void> main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
@@ -38,15 +29,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'TruckHub',
-        theme: ThemeData(
-          navigationBarTheme: NavigationBarThemeData(
-            labelTextStyle: MaterialStatePropertyAll(const TextStyle()
-                .decorateTextStyle(
-                    color: blackColor,
-                    fontWeight: fontWeight7,
-                    fontSize: fontSize2)),
+      debugShowCheckedModeBanner: false,
+      title: 'TruckHub',
+      theme: ThemeData(
+        navigationBarTheme: NavigationBarThemeData(
+          labelTextStyle: MaterialStatePropertyAll(
+            const TextStyle().decorateTextStyle(
+              color: blackColor,
+              fontWeight: fontWeight7,
+              fontSize: fontSize2
+            )
           ),
         ),
 
@@ -55,7 +47,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const SvgFlut()
+      home: const GoogleMapView()
     );
   }
 }
